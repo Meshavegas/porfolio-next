c54dda1fa1106c77d9da8227c0bd9ff38b14c8a2
Mesha Vegas <84249301+Meshavegas@users.noreply.github.com>
2023-12-07 10:00:23 +0100

    Update page.tsx

--- app/(user)/page.tsx
+++ app/(user)/page.tsx
@@ -2,13 +2,21 @@ import Image from "next/image";
 import SectionLayout from "../Component/SectionLayout";
 import HeroSection from "../Component/HeroSection";
 import AboutMe from "../Component/AboutMe";
+import SkillSection from "../Component/SkillSection";
+import ProjectsScetion from "../Component/ProjectsScetion";
 
 export default function Home() {
   return (
     <main>
       <HeroSection />
-      <SectionLayout classNames="" sectionId="Hero" hauteur="90vh">
+      <SectionLayout classNames="" sectionId="about" hauteur="90vh">
         <AboutMe />
+      </SectionLayout>
+      <SectionLayout classNames=" bg-whited" sectionId="skill" hauteur="90vh">
+        <SkillSection />
+      </SectionLayout>
+      <SectionLayout classNames=" bg-whited" sectionId="project" hauteur="90vh">
+        <ProjectsScetion />
       </SectionLayout>
     </main>
   );

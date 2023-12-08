dc884992ff85a880a76f2e054794b62e65314ed6
Mesha Vegas <84249301+Meshavegas@users.noreply.github.com>
2023-12-08 09:40:32 +0100

    Update AboutCard.tsx

--- app/Component/AboutCard.tsx
+++ app/Component/AboutCard.tsx
@@ -22,12 +22,12 @@ export const AboutCard = () => {
           Bienvenue dans mon univers ! Je suis{" "}
           <span className=" font-bold uppercase">lontchi loic steve</span>, un
           passionné du code avec une formation de qualité dans une école
-          supérieure de renom. Mon expertise exceptionnelle s'étend du
-          JavaScript au Java et au-delà, résultat d'une expérience
+          supérieure de renom. Mon expertise exceptionnelle s&lsquo;étend du
+          JavaScript au Java et au-delà, résultat d&lsquo;une expérience
           professionnelle variée et enrichissante.
         </p>
         <p className="mt-3">
-          Mon parcours m'a confronté à une diversité de défis, des projets
+          Mon parcours m&lsquo;a confronté à une diversité de défis, des projets
           innovants aux solutions créatives pour des problèmes complexes.
           Au-delà de mes compétences techniques, je valorise la communication
           transparente et la collaboration harmonieuse au sein des équipes.

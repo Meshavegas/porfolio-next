b10ce286f05dbb9a108e52669aa1f9cf649873f8
Mesha Vegas <84249301+Meshavegas@users.noreply.github.com>
2023-12-09 01:22:19 +0100

    Update HeroSection.tsx

--- app/Component/HeroSection.tsx
+++ app/Component/HeroSection.tsx
@@ -11,10 +11,13 @@ const HeroSection = () => {
           Work better, faster, Together
         </h1>
         <p className=" text-xl text-justify mt-10 leading-10 text-white">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam a
-          reiciendis sed deserunt vitae error doloribus omnis neque? Rerum,
-          excepturi hic. Fugiat necessitatibus enim consequuntur ad consectetur
-          minima quibusdam omnis
+          Embark on a journey through the boundless realm of technological
+          versatility with us. I am more than just a developer – I am a digital
+          architect, an experience creator, and an unwavering problem solver.
+          From intuitive design to robust code, I seamlessly blend aesthetics
+          with functionality, sculpting innovative solutions with every line of
+          code. Welcome to a world where versatility becomes a technological
+          masterpiece. Together, let's push the boundaries of what's possible.
         </p>
         <button className=" mt-10 bg-secondary rounded-full px-6 py-3 text-white text-xl">
           Let&lsquo;s Talk!

052214b148788b0d62705ef7713b7cd85aa12c3d
Mesha Vegas <84249301+Meshavegas@users.noreply.github.com>
2023-12-10 18:11:43 +0100

    Update BlogSection.tsx

--- app/Component/BlogSection.tsx
+++ app/Component/BlogSection.tsx
@@ -2,7 +2,16 @@ import React from "react";
 import Image from "next/image";
 import { CiCalendarDate } from "react-icons/ci";
 import { FaCommentDots } from "react-icons/fa6";
-const BlogSection = () => {
+import urlFor from "@/lib/urlFor";
+import { groq } from "next-sanity";
+import { client } from "@/sanity/lib/client";
+const query = groq`
+*[_type =='post']{
+  ...,
+  category[]->,
+}`;
+const BlogSection = async () => {
+  const posts: Post[] = await client.fetch(query);
   return (
     <div>
       <div className=" text-center">
@@ -12,43 +21,8 @@ const BlogSection = () => {
         </h1>
       </div>
       <div className=" flex justify-between flex-wrap mt-5 ">
-        {blogs.map((blog, key) => (
-          <div
-            className=" w-full md:w-[32%] mt-5 rounded-xl overflow-hidden shadow-lg"
-            key={key}
-          >
-            <Image
-              src={blog.imageUrl}
-              alt={blog.imageUrl}
-              className=" object-cover aspect-video"
-              width={500}
-              height={300}
-            />
-            <div className="bg-white px-4 py-4">
-              <div className=" flex justify-between text-sm">
-                <p className=" flex items-center justify-between">
-                  {blog.thematique.substring(0, 15)}
-                </p>
-                <p className=" flex items-center justify-between">
-                  <CiCalendarDate className=" text-primary mr-2" />
-                  {new Date(blog.date).toLocaleDateString("fr-FR", {
-                    day: "numeric",
-                    month: "numeric",
-                    year: "numeric",
-                  })}
-                </p>
-                <p className=" flex items-center justify-between">
-                  <FaCommentDots className=" text-primary mr-2" />
-                  {blog.comment}.comments
-                </p>
-              </div>
-              <h1 className=" text-xl mt-2"> {blog.title}</h1>
-              <p className=" mt-2 mb-6">{blog.description}</p>
-              <a href="#" className=" underline text-xl text-primary">
-                Read more
-              </a>
-            </div>
-          </div>
+        {posts.map((blog, key) => (
+          <BlogCard key={key} blog={blog} index={key} />
         ))}
       </div>
     </div>
@@ -57,41 +31,55 @@ const BlogSection = () => {
 
 export default BlogSection;
 
-const blogs = [
-  {
-    title: "How to make your website SEO friendly",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam quae voluptate e",
-    imageUrl: "/infographie.png",
-    date: new Date(),
-    comment: 12,
-    thematique: "SEO|Web Developement",
-  },
-  {
-    title: "How to make your website SEO friendly",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam quae voluptate e",
-    imageUrl: "/infographie.png",
-    date: new Date(),
-    comment: 12,
-    thematique: "SEO|Web Developement",
-  },
-  {
-    title: "How to make your website SEO friendly",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam quae voluptate e",
-    imageUrl: "/infographie.png",
-    date: new Date(),
-    comment: 12,
-    thematique: "SEO|Web Developement",
-  },
-  {
-    title: "How to make your website SEO friendly",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam quae voluptate e",
-    imageUrl: "/infographie.png",
-    date: new Date(),
-    comment: 12,
-    thematique: "SEO|Web Developement",
-  },
-];
+function BlogCard({ index, blog }: { index: number; blog: Post }) {
+  // console.log(blog);
+
+  return (
+    <div
+      className=" w-full md:w-[32%] mt-5 rounded-xl overflow-hidden shadow-lg"
+      key={index}
+    >
+      <Image
+        src={urlFor(blog.mainImage).url()}
+        alt={blog.slug.current}
+        className=" object-cover aspect-video hover:scale-110 transition duration-500 transform "
+        width={500}
+        height={300}
+      />
+      <div className="bg-white px-4 py-4">
+        <div className=" flex justify-between text-sm">
+          <p className=" flex items-center justify-between">
+            {blog.category.map((value, key) => {
+              return (
+                <>
+                  {blog.category.length - 1 > key ? (
+                    <span key={key}>{value.title.substring(0, 15)} | </span>
+                  ) : (
+                    <span key={key}>{value.title.substring(0, 15)}</span>
+                  )}
+                </>
+              );
+            })}
+          </p>
+          <p className=" flex items-center justify-between">
+            <CiCalendarDate className=" text-primary mr-2" />
+            {new Date(blog._createdAt).toLocaleDateString("fr-FR", {
+              day: "numeric",
+              month: "numeric",
+              year: "numeric",
+            })}
+          </p>
+          <p className=" flex items-center justify-between">
+            <FaCommentDots className=" text-primary mr-2" />
+            {blog.comments?.length ? blog.comments?.length : 0} .comments
+          </p>
+        </div>
+        <h1 className=" text-xl mt-2"> {blog.title}</h1>
+        <p className=" mt-2 mb-6 line-clamp-4">{blog.description}</p>
+        <a href="#" className=" underline text-xl text-primary">
+          Read more
+        </a>
+      </div>
+    </div>
+  );
+}
